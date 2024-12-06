76997a2cb63e22306656285b2fd404a7328d3241
anyelo <anyelobg.dev@gmail.com>
2024-12-06 04:14:38 -0500

    Update homepage greeting and restructure navigation; change welcome message to Spanish and adjust layout for dashboard link

--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,16 @@ import { ModeToggle } from "@/components/ui/toggle-theme";
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start text-center sm:text-left">
         <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
-          Welcome to <span className="text-blue-600">Delivery Dynamics</span>
+          Bienvenido a <span className="text-blue-600">Delivery Dynamics</span>
         </h1>
-        <Link href="/dashboard" className="mx-auto">
-          <Button>Get Started</Button>{" "}
-        </Link>
-        <ModeToggle />
+        <div className="w-full flex justify-center items-center  gap-x-5 flex-wrap">
+          <Link href="/routes">
+            <Button>Dashboard</Button>
+          </Link>
+          <ModeToggle />
+        </div>
       </main>
     </div>
   );
